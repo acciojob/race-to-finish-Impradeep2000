c80111dd780f217950ca727ce4eacc3438333930
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-28 18:07:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-
-function getRandomDelay() {
-  return Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
-}
-window.promises = [new Promise((resolve) => setTimeout(() => resolve("Promise 1 resolved"), getRandomDelay())),
-  new Promise((resolve) => setTimeout(() => resolve("Promise 2 resolved"), getRandomDelay())),
-  new Promise((resolve) => setTimeout(() => resolve("Promise 3 resolved"), getRandomDelay())),
-  new Promise((resolve) => setTimeout(() => resolve("Promise 4 resolved"), getRandomDelay())),
-  new Promise((resolve) => setTimeout(() => resolve("Promise 5 resolved"), getRandomDelay()))];
-
-Promise.any(promises)
-  .then((result) => {
-    // Print the result to the output div
-    const outputDiv = document.getElementById("output");
-    outputDiv.textContent = result;
-  })
-  .catch((error) => {
-    console.error("All promises rejected:", error);
-  });
+window.promises = [];
 // Do not change the code above this
+
 // add your promises to the array `promises`
+let x = Math.floor(Math.random() * 5) + 1;
+console.log(x);
+let promise1 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("1");
+	}, x * 1000)
+});
+
+let promise2 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("2");
+	}, x * 1000)
+})
+	let promise3 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("3");
+	}, x * 1000)
+})
+	let promise4 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("4");
+	}, x * 1000)
+})
+	let promise5 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("5");
+	}, x * 1000)
+})
+
+	promises.push(promise1);
+	promises.push(promise2);
+	promises.push(promise3);
+	promises.push(promise4);
+	promises.push(promise5);
+
+	Promise.any(promises).then((value) => {
+    document.getElementById("output").innerText = value;
+  });
